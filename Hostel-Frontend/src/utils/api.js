586a2e2eb903a91d2eeecd15d5f81263bf9b70d0
Nancy Wangui Kanyingi <wangui.kanyingi2015@gmail.com>
2025-11-25 16:03:46 +0300

    Added render url

--- Hostel-Frontend/src/utils/api.js
+++ Hostel-Frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 // src/utils/api.js
-export const API_BASE_URL = 'http://localhost:5000';
+export const API_BASE_URL = 'https://hostel-hunt-a0v4.onrender.com';
 
 /**
  * Helper function to make API requests
